2c2e61c9939e218a62fd52b7ff4cf71d228795e6
amirziai <arziai@gmail.com>
2018-01-05 19:24:12 -0800

    attending workshops init

---
+++ cpp/attending-workshops.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+//Define the structs Workshops and Available_Workshops.
+//Implement the functions initialize and CalculateMaxWorkshops
+
+int main(int argc, char *argv[]) {
+    int n; // number of workshops
+    cin >> n;
+    // create arrays of unknown size n
+    int* start_time = new int[n];
+    int* duration = new int[n];
+
+    for(int i=0; i < n; i++){
+        cin >> start_time[i];
+    }
+    for(int i = 0; i < n; i++){
+        cin >> duration[i];
+    }
+
+    Available_Workshops * ptr;
+    ptr = initialize(start_time,duration, n);
+    cout << CalculateMaxWorkshops(ptr) << endl;
+    return 0;
+}
